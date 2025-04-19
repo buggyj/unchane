8214310687f258925333bdbcf8851d6c6e7eef9f
buggyj <buggyjef@gmail.com>
2025-04-19 11:13:29 +0200

    restrict reload to non system and ai modulesa

--- dev/reload.mjs
+++ dev/reload.mjs
@@ -1,10 +1,12 @@
 const {html, render } = await import ("$:/plugins/bj/unchane/preactsignal.mjs");
 
 const {init} = await import ("$:/plugins/bj/unchane/towidget.mjs")
-
 const bjModuleLoader = await import ("$:/plugins/bj/unchane/mimport.js");
-
 const {setModuleP} = await import ("$:/plugins/bj/unchane/modulep.js");
+
+const {filterTiddlers} = await import ("$:/plugins/bj/unchane/storeutils.js")
+const {getTextReference,setTextReference} = await import ("$:/plugins/bj/unchane/store.js")
+
 
 export function start({__pwidget}) {
 	const {invokeActionString} = init(__pwidget)//methods bound to widget instance 
@@ -12,10 +14,21 @@ export function start({__pwidget}) {
 	function resetWidgetTree() {return `<$action-setfield $tiddler="$:/temp/bj/unchane/dev/resetTid" update="update"/>`}
 	
 	function doReset() {
-		bjModuleLoader.bjModuleLoader.reset() 
+		let modulesfiles = filterTiddlers("[all[shadows+tiddlers]suffix[.mjs]!prefix[$:/plugins/bj/unchane]!prefix[$:/plugins/bj/simplifai]]")
+		for (const element of modulesfiles) {
+			bjModuleLoader.bjModuleLoader.del(element) 
+			console.log(element);
+		}
+		
 		setModuleP(null) 
 		//touch the reset macro to make widget tree re-render
-		invokeActionString(resetWidgetTree())	
+		//invokeActionString(resetWidgetTree())	
+		let story = getTextReference("$:/StoryList!!list")
+		setTextReference("$:/StoryList!!list","")
+		
+		setTimeout(() => {
+			setTextReference("$:/StoryList!!list",story)
+		}, 500)
 	}	
 	return  html`
 	  <button onClick=${() => (doReset())}>
@@ -23,3 +36,5 @@ export function start({__pwidget}) {
 	  </button>`
 }
 export var psignals = []
+
+
